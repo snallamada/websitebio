1dc54e947710b496a6d317df267abaf00c2f97f9
snallamada <shravanreddynallamada@gmail.com>
2024-01-02 10:29:03 +0100

    ADDED AND REPLACED NAME

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -11,7 +11,7 @@ const ContactLeft = () => {
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
+        <h3 className="text-3xl font-bold text-white">Shravan</h3>
         <p className="text-lg font-normal text-gray-400">
           MERN Stack Developer
         </p>
