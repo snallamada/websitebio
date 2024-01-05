9aff00325cb14c5efe97c4d9e66fe6dd0d302666
snallamada <shravanreddynallamada@gmail.com>
2024-01-05 21:51:04 +0100

    add code

--- src/components/resume/Education.js
+++ src/components/resume/Education.js
@@ -17,23 +17,40 @@ const Education = () => {
         </div>
         <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
           <ResumeCard
-            title="Bac+5"
+            title="Master's in Digital Marketing Strategy (Bac+5)"
             subTitle="Ecole de Management Léonard De Vinci.(2019 - 2020)"
             result="France,Paris"
-            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-          />
+            des={<ul>
+              <li>Assisted in research projects related to emerging trends in web development, gaining exposure to the latest technologies and frameworks.</li>
+              <li>Collaborated with faculty on website optimization initiatives, implementing responsive design and accessibility features.</li>
+              <li>Led a team in the development of a dynamic website for a local business, integrating e-commerce functionalities and ensuring optimal user experience.
+</li>
+            </ul>
+
+            } />
           <ResumeCard
-            title=" Bachelor's in Cumputer Science Engineering"
+            title=" Bachelor's in Cumputer Science Engineering."
             subTitle="Jawaharlal Nehru Technological University Hyderabad (2014 - 2016)"
             result="Hyderabad,India"
-            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-          />
+            des={
+            <ul>
+              <li>Contributed to the development of web applications, focusing on front-end and back-end technologies.</li>
+              <li>Enhanced proficiency in HTML, CSS, and JavaScript for creating responsive and visually appealing user interfaces.</li>
+              <li>Worked with Node.js and Express for server-side development, implementing RESTful APIs.</li>
+              <li>Designed and developed a personal portfolio website showcasing projects and skills using HTML, CSS, and JavaScript.</li>
+              </ul>
+            }
+            />
           <ResumeCard
             title="Intermidiate +2"
             subTitle="Vidwan Junior College(2010 - 2012)"
             result="Nalgonda,India"
-            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
-          />
+            des={
+              <ul>
+                <li>Embraced challenges, learned from failures, and developed a resilient mindset for future pursuits.</li>
+                <li>In summary, my +2 education was a well-rounded experience, marked by academic achievements, extracurricular engagement, and personal growth, providing a strong foundation for future endeavors.</li>
+                </ul>
+              }/>
         </div>
       </div>
       
--- src/components/resume/TechnicalSkills.js
+++ src/components/resume/TechnicalSkills.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { motion } from "framer-motion";
 import ResumeCard from "./ResumeCard";
+import {siJavascript} from "react-icons/si"
 const TechnicalSkills = () => {
     return (
         <motion.div
@@ -16,22 +17,41 @@ const TechnicalSkills = () => {
             </div>
             <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
               <ResumeCard
-                title="Front End & FrameWorks"
-                subTitle=""
-                result="Frontend"
-                des="Html, CSS, Javascript, Typescript, ReactJs, VueJs, AngulerJS,UX Design&Architecture"
+                title="Front End"
+                subTitle="Core technologies for Develop Website, Web Application"
+                result="Presentation"
+                // des="Html, CSS, Javascript, Typescript, ReactJs, VueJs, AngulerJS,UX Design & Architecture"
+                des={
+                <ul>
+                  <li>Html, CSS</li>
+                  <li>JavaScript, Typescript</li>
+                  <li>React, AngulerJS, VueJs</li>
+                 </ul>
+                }
               />
               <ResumeCard
-                title=" Bachelor's in Cumputer Science Engineering"
-                subTitle="Jawaharlal Nehru Technological University Hyderabad (2014 - 2016)"
-                result="Hyderabad,India"
-                des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+                title="Libraries"
+                subTitle="Reusable code libraries"
+                result="UI design"
+                des="Bootstrap , React Bootstrap, ANT, Tailwind"
               />
               <ResumeCard
-                title="Intermidiate +2"
-                subTitle="Vidwan Junior College(2010 - 2012)"
-                result="Nalgonda,India"
-                des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+                title="Back End"
+                subTitle="Server-side technologies"
+                result="API"
+                des="Strapi, NodeJS,ExpressJs,API's, GraphQL."
+              />
+              <ResumeCard
+                title="Database"
+                subTitle="Systems for storing, managing, and retrieving data"
+                result="Data Storage"
+                des="MongoDB, Mysql, PostgreSQL, FireBase."
+              />
+               <ResumeCard
+                title="Mobile Application"
+                subTitle="Applications for Mobile Devices"
+                result="iOS or Android."
+                des="React Native (Android, ios ),Expo(view)"
               />
             </div>
           </div>
@@ -45,22 +65,34 @@ const TechnicalSkills = () => {
             </div>
             <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
               <ResumeCard
-                title="Front-end Developer"
-                subTitle="Publication Agora France - (2021 - Present)"
-                result="France, Paris"
-                des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+                title="Cloud Services"
+                subTitle="Web-based services"
+                result="Web Housting"
+                des="WPserveur, AWS, Azure, DigitalOcean"
               />
               <ResumeCard
-                title="Web Developer - Intern"
-                subTitle="Daylindo - (2020 - 6 months)"
-                result="France, Paris"
-                des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+                title="CMS"
+                subTitle="Content Management Systems"
+                result="Websites"
+                des="WordPress, JQuery, web hosting servers, Cpanel,Payment gateways and theme customizations."
               />
               <ResumeCard
-                title="Web Developer"
-                subTitle="WI - Group Private Limited - (2016 - 2019)"
-                result="India, Guntur"
-                des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+                title="Tools"
+                subTitle="Web-based platforms"
+                result="Version Control"
+                des="Github, Bitbucket,postman,Docker, kubernetes"
+              />
+                <ResumeCard
+                title="Management Tools"
+                subTitle="collaboration"
+                result="Communication"
+                des="Slack, Miro, MS office 365, Zoom,Agile,Scrum"
+              />
+                 <ResumeCard
+                title="Design"
+                subTitle="Principles, tools, and processes"
+                result="creating visually appealing"
+                des="Figma, Adobe"
               />
             </div>
           </div> 
