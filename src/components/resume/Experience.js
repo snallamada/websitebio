7806e9e966e052fff5a6b664a3ca3305cdaea691
snallamada <shravanreddynallamada@gmail.com>
2024-01-05 17:08:10 +0100

    update Experience page

--- src/components/resume/Experience.js
+++ src/components/resume/Experience.js
@@ -21,23 +21,37 @@ const Experience = () => {
                 result="France, Paris"
                 des={
                 <ul>
-                  <li>- Actively participate in team-oriented hiring processes.</li>
-                  <li>Demonstrate a commitment to fostering a positive workplace environment.</li>
-                  <li>Collaborate effectively with cross-functional teams.</li>
+                  <li>Maintain and improve UI/UX of websites, and web applications - updates, modifications, security, GDPR, cookie consent, etc.</li>
+                  <li>Backend, Server setup/monitor, API integration, configuration & development of live projects.</li>
+                  <li>Live-content Feature: developed by using ReactJs to fetch data and render personalized content.</li>
+                  <li>Live stats Data web application developed with the language variables dashboard.</li>  
+                  <li>Creating new websites for special events which includes Dynamic Features.</li>   
+                  <li>Addition of new features, functionalities, and live content, maintain FTP, Cpanel & CMS platforms, Create a plugin with ReactJs, web application.</li>         
                 </ul>
               } />
               <ResumeCard
                 title="Web Developer - Intern"
                 subTitle="Daylindo - (2020 - 6 months)"
                 result="France, Paris"
-                des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
-              />
+                des={
+                  <ul>
+                    <li>Developed application with ReactJS , Redux, and AngularJS.</li>
+                    <li>Technologies adopted: npm,git, jwt Tokens, Google Cloud, firebase</li>
+                    <li>Revamped and maintained the company website including authentication.</li>
+                   </ul>
+                }  />
               <ResumeCard
                 title="Web Developer"
                 subTitle="WI - Group Private Limited - (2016 - 2019)"
                 result="India, Guntur"
-                des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
-              />
+                des={
+                  <ul>
+                    <li>Worked on UI / UX design principles relating to Web applications and websites, debugging website issues.</li>
+                    <li>Worked and developed web applications, adding customized features depending on the client.</li>
+                    <li>Fix security issues and debugging vulnerabilities on both the front end and back end.</li>
+                    <li>Developed websites from landing pages to e-commerce sites.</li>
+                   </ul>
+                }  />
             </div>
           </div>
         </motion.div>
