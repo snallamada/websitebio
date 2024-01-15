f8be26644d2e4a9d36c6f12b38db0dcc7a1e3507
snallamada <shravanreddynallamada@gmail.com>
2024-01-15 21:46:28 +0100

    added images curd

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Title from '../layouts/Title'
-import { projectOne, projectTwo, projectThree } from "../../assets/index";
+import { projectOne, projectTwo, projectThree , projectFour} from "../../assets/index";
 import ProjectsCard from './ProjectsCard';
+import { FaReact , FaAngular } from 'react-icons/fa';
 
 const Projects = () => {
   return (
@@ -20,14 +21,16 @@ const Projects = () => {
           title="Employee Portal"
           des="The Employee Portal streamlines access to company information through secure logins, offering a centralized hub for updates and documents. It promotes efficiency, ensuring quick retrieval of essential information, and enhances communication in the workplace."
           src={projectOne}
+          framework={<FaReact/>}
           gitlink="https://github.com/snallamada/Employee-portal-app"
           weblink="https://portallogin.netlify.app/" 
-          
+ 
         />
         <ProjectsCard
           title="Food search app"
           des="The Food Search App simplifies your culinary exploration, allowing you to discover a diverse range of dishes. From pizza and pasta to biryani and more, easily find your desired food items. Enjoy a seamless and delightful food discovery experience at your fingertips."
           src={projectTwo}
+          framework={<FaReact/>}
           gitlink="https://github.com/snallamada/food-search-app"
           weblink="https://shravan-food-search.netlify.app/" 
        />
@@ -35,16 +38,19 @@ const Projects = () => {
           title="Task Tracker App"
           des="The Task Tracker App streamlines your productivity journey, offering a user-friendly platform to manage your tasks effortlessly. Organize, track, and accomplish your goals with ease. Enhance your efficiency and stay on top of your to-do list, making progress one task at a time."
           src={projectThree}
+          framework={<FaReact/>}
           gitlink="https://github.com/snallamada/Task-tracker-app"
           weblink="https://shravan-task-tracker-app-one.vercel.app/" 
         />
-        {/* <ProjectsCard
-          title="SOCIAL MEDIA CLONE"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
-          src={projectThree}
+       <ProjectsCard
+          title="CURD Application"
+          des="Create, read, update, and delete tasks seamlessly with our intuitive CRUD application. Streamline task management for enhanced productivity and efficiency."
+          src={projectFour}
+          framework={<FaAngular/>}
+          gitlink="https://github.com/snallamada/anguler-curd-app"
+          weblink="https://shravan-anguler-curd-app.vercel.app/" 
         />
-        <ProjectsCard
+       {/*   <ProjectsCard
           title="E-commerce Website"
           des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BsGithub } from "react-icons/bs";
-import { FaGlobe } from "react-icons/fa";
+import { FaGlobe  } from "react-icons/fa";
 
-const ProjectsCard = ({ title, des, src, gitlink, weblink }) => {
+const ProjectsCard = ({ title, des, src, gitlink, weblink, framework }) => {
   return (
     <div className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">
 
@@ -22,6 +22,16 @@ const ProjectsCard = ({ title, des, src, gitlink, weblink }) => {
               {title}
             </h3>
             <div className="flex gap-2">
+            <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
+                <a href={gitlink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer"
+              >
+                {framework}
+              </a>  
+              </span>
+ 
               <span className="text-lg w-10 h-10 rounded-full bg-black inline-flex justify-center items-center text-gray-400 hover:text-designColor duration-300 cursor-pointer">
                 <a href={gitlink}
                 target="_blank"
