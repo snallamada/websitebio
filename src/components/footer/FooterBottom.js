00e167255307345c1fbb0dfd72a1d0303dd930a9
snallamada <89526628+snallamada@users.noreply.github.com>
2025-01-18 15:30:11 -0500

    removed c from the footer and updated instead @

--- src/components/footer/FooterBottom.js
+++ src/components/footer/FooterBottom.js
@@ -5,7 +5,7 @@ const FooterBottom = () => {
     <div className="w-full py-10">
       <p className="text-center text-gray-500 text-base">
       
-        <span>&copy; @ {new Date().getFullYear()} All rights reserved by Shravan NALLAMADA.</span>
+        <span>@ {new Date().getFullYear()} All rights reserved by Shravan NALLAMADA.</span>
 
       </p>
     </div>
